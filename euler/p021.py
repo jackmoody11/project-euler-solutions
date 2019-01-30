1d21a11b79b5072ed9200b11a8be91e02a3006fa
jackmoody11 <jackmoody@unc.edu>
2019-01-30 19:24:15 +0100

    Solve #21

---
+++ euler/p021.py
@@ -0,0 +1,21 @@
+from math import sqrt
+
+
+def d(n):
+    divisors = []
+    for i in range(1, n // 2 + 1):
+        if n % i == 0:
+            divisors.append(i)
+    return sum(divisors)
+
+
+def amicables(n):
+    ams = []
+    for i in range(1, n + 1):
+        di = d(i)
+        ddi = d(di)
+        if i == ddi != di:
+            ams.append(i)
+    return ams
+
+print(sum(amicables(10_000)))
