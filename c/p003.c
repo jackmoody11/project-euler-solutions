d23aab5e8a3f45d76b5eaf90be9633f57cd2aaf0
jackmoody11 <jackmoody@unc.edu>
2019-03-12 10:11:09 +0100

    Use utils for reusable functions

--- c/p003.c
+++ c/p003.c
@@ -1,38 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int is_prime(long int n)
-{
-    if (n < 2)
-    {
-        return 0;
-    }
-    long last = (long) sqrt(n) + 1;
-
-    for (long j = 2; j <= last; ++j)
-    {
-        if (0 == n % j)
-        {
-        return 0;
-        }
-    }
-    return 1;
-}
-
-long max_prime_factor(long n)
-{
-    long max_possible = (long) sqrt(n) + 1;
-    long max_div;
-    for(long i = 2; i <= max_possible; i++)
-    {
-        if (n % i == 0 && is_prime(i)) 
-        {
-            max_div = i;
-        }
-    }
-    return max_div;
-    
-}
+#include "utils.h"
 
 int main()
 {
---
+++ c/utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+int is_prime(int n); // Test if int is prime
+long max_prime_factor(long n); // Find max prime factor
+#endif /* UTILS_H */
