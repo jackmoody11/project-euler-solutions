15fe4fb15356bf929489b0e20436069e09cc4bc6
jackmoody11 <jackmoody@unc.edu>
2019-03-12 14:30:03 +0100

    Solve #10

---
+++ c/p010.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main()
+{
+    long s = 0;
+    for(long i = 2; i <= 2000000; i++)
+    {
+        if (is_prime(i)) {
+            s += i;
+        }
+    }
+    printf("%ld\n", s);
+}
