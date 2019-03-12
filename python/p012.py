f9886cc3c208edf05b01b9bd95c55e89df718632
jackmoody11 <jackmoody@unc.edu>
2019-03-12 23:20:57 +0100

    Solve #12

--- python/p012.py
+++ python/p012.py
@@ -3,25 +3,29 @@
 from math import sqrt
 
 
-def find_divisors(n):
-    divisors = []
-    for i in range(1, n + 1):
-        if n % i == 0:
-            divisors.append(i)
-    return divisors
+def num_divisors(n):
+	end = int(sqrt(n))
+    # ex: 10 has divisors 1 2 5 10
+    # 1 2 | 5 10 -> 2 captured * 2 = 4 divisors
+    # ex: 16 has divisor 1 2 4 | 8 16 -> 3 captured * 2 = 6,
+    # but 4 should only be counted once
+
+	result = sum(2
+		for i in range(1, end + 1)
+		if n % i == 0)
+	if end**2 == n:
+		result -= 1
+	return result
 
 
 def compute():
-    # We know that to have 500 divisors, num >= 500
-    # i * (i + 1) / 2 is >= 500 when i = 32, so start there
-    i = 7
+    i, num = 1, 1
     while True:
-        num = sum(range(i))
-        divisors = find_divisors(num)
-        if len(divisors) > 500:
+        if num_divisors(num) > 500:
             return num
         else:
             i += 1
+            num += i
 
 
 if __name__ == '__main__':
