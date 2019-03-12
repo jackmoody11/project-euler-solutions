762ac1c16db079c81a358eec07fac3e9ae4a9bb9
jackmoody11 <jackmoody@unc.edu>
2019-03-12 14:21:15 +0100

    Solve #9

---
+++ c/p009.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+
+long find_ab();
+
+int main()
+{
+    long res = find_ab();
+    printf("%ld\n", res);    
+}
+
+long find_ab()
+{
+    for(int a = 1; a < 500; a++)
+    {
+        for(int b = 1; b < 500; b++)
+        {
+            if (a + b + sqrt(pow(a, 2) + pow(b, 2)) == 1000) {
+                return (a * b * sqrt(pow(a, 2) + pow(b, 2)));
+            }
+        }
+    }
+    return 0;
+}
