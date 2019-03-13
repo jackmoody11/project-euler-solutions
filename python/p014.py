afd2ecdaeb3c157764b2638644da73726037ac2c
jackmoody11 <jackmoody@unc.edu>
2019-03-13 15:32:51 +0100

    Solve #14

--- python/p014.py
+++ python/p014.py
@@ -1,8 +1,6 @@
 # Find starting number (between 1 and 1,000,000) that
 # has the longest collatz string (iterations to 1)
 
-# Unsolved - need to optimize algorithm (takes too long)
-
 def length_collatz(n):
     i = 1
     while True:
@@ -16,14 +14,12 @@ def length_collatz(n):
 
 
 def compute():
-    max = (0, 0)  # (starting value, number of steps)
-    for i in range(1_000_001):
+    my_max = (0, 0)  # (starting value, number of steps)
+    for i in range(1, 1_000_001):
         l = length_collatz(i)
-        if l > max[1]:
-            max = (i, l)
-        if i % 10_000 == 0:
-            print(i, l)
-    return max[0]
+        if l > my_max[1]:
+            my_max = (i, l)
+    return my_max[0]
 
 
 if __name__ == '__main__':
