09b2577446679d8f3d9f66e2587b0f8e47d5324b
jackmoody11 <jackmoody@unc.edu>
2019-03-13 12:30:11 +0100

    Reduce loop range to speed up algorithm

--- python/p012.py
+++ python/p012.py
@@ -7,14 +7,14 @@ def num_divisors(n):
 	end = int(sqrt(n))
     # ex: 10 has divisors 1 2 5 10
     # 1 2 | 5 10 -> 2 captured * 2 = 4 divisors
-    # ex: 16 has divisor 1 2 4 | 8 16 -> 3 captured * 2 = 6,
-    # but 4 should only be counted once
+    # ex: 16 has divisor 1 2 | 4 8 16 -> 3 captured * 2 = 6,
+    # but 4 also be counted once
 
 	result = sum(2
-		for i in range(1, end + 1)
+		for i in range(1, end)
 		if n % i == 0)
 	if end**2 == n:
-		result -= 1
+		result += 1
 	return result
 
 
