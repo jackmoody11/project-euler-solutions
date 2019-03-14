7ff5d4bedb900be0ffda313745f30773f759d979
jackmoody11 <jackmoody@unc.edu>
2019-03-14 10:46:05 +0100

    Continue work on #23

--- python/p023.py
+++ python/p023.py
@@ -1,22 +1,20 @@
 # Currently incorrect -- needs work
 
 from itertools import combinations
+from math import sqrt
 
-
-def is_abundant(n):
-    div_sum = 0
-    for i in range(1, n // 2 + 1):
+def proper_divisors(n):
+    divs = set()
+    for i in range(1, n // 2):
         if n % i == 0:
-            div_sum += i
-            if div_sum > n:
-                return True
-    return False
-
+            divs.add(i)
+            divs.add(n // i)
+    return divs.difference({n})
 
 def list_abundants(n):
     abundants = []
     for i in range(12, n + 1):
-        if is_abundant(i):
+        if sum(proper_divisors(i)) > i:
             abundants.append(i)
     return abundants
 
@@ -24,4 +22,9 @@ def list_abundants(n):
 def compute():
     combos = list(combinations(list_abundants(28123), 2))
     sms = {sum(i) for i in combos}
+    res = sum([j for j in range(1, 28124) if j not in sms])
     return sum([j for j in range(1, 28124) if j not in sms])
+
+
+if __name__ == '__main__':
+    print(compute())
