e38d21129230019d5bc4bf57a53d590dc19dd18f
jackmoody11 <jackmoody@unc.edu>
2019-03-15 23:11:49 +0100

    Slight performance improvement with use of generators over lists

--- python/p011.py
+++ python/p011.py
@@ -6,7 +6,6 @@ filename = "p011.txt"
 with open(filename) as f:
     matrix = [list(map(int, line.split())) for line in f]
 
-
 def compute():
     # Start in the top left corner of the matrix
     # Go from left to right and (if possible) check 
@@ -17,18 +16,18 @@ def compute():
     max_p = 0
     for r in range(len(matrix) - 4):
         for c in range(len(matrix)):
-            down = prod([matrix[r-i][c] for i in range(4)])
+            down = prod((matrix[r-i][c] for i in range(4)))
             if c >= 3:
-                left_diag = prod([matrix[r+i][c-i] for i in range(4)])
+                left_diag = prod((matrix[r+i][c-i] for i in range(4)))
             if c <= 16:
-                right = prod([matrix[r][c+i] for i in range(4)])
-                right_diag = prod([matrix[r+i][c+i] for i in range(4)])
+                right = prod((matrix[r][c+i] for i in range(4)))
+                right_diag = prod((matrix[r+i][c+i] for i in range(4)))
             if c >=3 and c<= 16:
-                m = max([down, left_diag, right, right_diag])
+                m = max((down, left_diag, right, right_diag))
             elif c < 3:
-                m = max([down, right, right_diag])
+                m = max((down, right, right_diag))
             elif c > 16:
-                m = max([down, left_diag])
+                m = max((down, left_diag))
             if m > max_p:
                 max_p = m
     return max_p
@@ -36,5 +35,3 @@ def compute():
 
 if __name__ == '__main__':
     print(compute())
-            
-            
