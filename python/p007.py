0cac23a12ffef3444ea96dcedfd731d155b421fc
jackmoody11 <jackmoody@unc.edu>
2019-03-15 22:55:15 +0100

    Make variables more readable

--- python/p007.py
+++ python/p007.py
@@ -1,16 +1,15 @@
 # Find 10,001 prime number
-from math import sqrt
 from utils import is_prime
 
 
 def nth_prime(n):
-    p = 0
-    x = 1
-    while p < n:
-        x += 1
-        if is_prime(x):
-            p += 1
-    return x
+    count = 0
+    number = 1
+    while count < n:
+        number += 1
+        if is_prime(number):
+            count += 1
+    return number
 
 
 if __name__ == '__main__':
