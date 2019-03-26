63655f6653ce723c1d41d18cb432c96a3bbeff22
jackmoody11 <jackmoody@unc.edu>
2019-03-27 00:14:16 +0100

    Solve #43

---
+++ python/p043.py
@@ -0,0 +1,24 @@
+from utils import prime
+from itertools import permutations
+
+PRIMES = [prime(i) for i in range(1, 8)]
+
+
+def follows_pattern(n):
+    for i in range(1, 8):
+        if int(str(n)[i:i+3]) % PRIMES[i-1] != 0:
+            return False
+    return True
+
+def compute():
+    
+    pattern_sum = 0
+    nine_pandigitals = [''.join(p) for p in permutations('0123456789') if p[0] != '0']
+    for pandigital in nine_pandigitals:
+        if follows_pattern(pandigital):
+            pattern_sum += int(pandigital)
+    return pattern_sum
+
+
+if __name__ == "__main__":
+    print(compute())
