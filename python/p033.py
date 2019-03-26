9657cf2e130e7e8f1bbffa6dc0e41b1d1fcd422c
jackmoody11 <jackmoody@unc.edu>
2019-03-26 19:36:54 +0100

    Solve #33

---
+++ python/p033.py
@@ -0,0 +1,30 @@
+from math import gcd
+
+def is_curious(a, b):
+    orig_frac = a / b
+    if a % 10 == 0 or b % 10 == 0:
+        return False
+    # Let sa and sb be the string versions of a and b
+    sa, sb = str(a), str(b)
+    # Check if any of the combinations of 
+    if sa[0] == sb[0] and orig_frac == int(sa[1]) / int(sb[1]):
+        return True
+    if sa[0] == sb[1] and orig_frac == int(sa[1]) / int(sb[0]):
+        return True
+    if sa[1] == sb[0] and orig_frac == int(sa[0]) / int(sb[1]):
+        return True
+    if sa[1] == sb[1] and orig_frac == int(sa[0]) / int(sb[0]):
+        return True
+    return False
+
+def compute():
+    num, denom = 1, 1
+    for a in range(11, 100):
+        for b in range(a+1, 100):
+            if is_curious(a, b):
+                num *= a
+                denom *= b
+    return int(denom / gcd(num, denom))
+    
+
+print(compute())
