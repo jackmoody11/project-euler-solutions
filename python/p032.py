c44403c50e629688b8361e9771a68cdf40cececf
jackmoody11 <jackmoody@unc.edu>
2019-03-26 18:55:08 +0100

    Solve #32

---
+++ python/p032.py
@@ -0,0 +1,36 @@
+def is_pandigital(n):
+    digits = len(n)
+    if digits >= 10:
+        return False
+    string_n = str(n)
+    for i in range(1, digits + 1):
+        if str(i) not in string_n:
+            return False
+    return True
+
+def is_nine_pandigital_product(a, b):
+    my_str = str(a) + str(b) + str(a*b)
+    if len(my_str) == 9 and is_pandigital(my_str):
+        return True
+    else:
+        return False
+
+def compute():
+    # 1 will never return a pandigital product (will repeat digits)
+    pandigitals = set()
+    # sqrt(987654321) = 31426.96, so we know this is the
+    # upper limit for our larger number (we define b as the larger number here)
+    for a in range(2, 31426):
+        for b in range(a, 31426):
+            mult = a * b
+            if len(str(a) + str(b) + str(mult)) > 9:
+                # Once the concatenation of a, b, and a + b gives
+                # a string of length > 9, we can skip to the next 
+                # value for a
+                break
+            if is_nine_pandigital_product(a, b) and mult not in pandigitals:
+                pandigitals.add(mult)
+    return sum(pandigitals)
+
+if __name__ == "__main__":
+    print(compute())
