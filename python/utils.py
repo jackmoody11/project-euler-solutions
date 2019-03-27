ec451b5ad04f534548a083c5b37c098cf8463f74
jackmoody11 <jackmoody@unc.edu>
2019-03-27 18:54:12 +0100

    Add supporting functions for #45

--- python/utils.py
+++ python/utils.py
@@ -11,6 +11,16 @@ def is_prime(n):
             return False
     return True
 
+def prime(n):
+    prime_count = 0
+    i = 1
+    while prime_count < n:
+        i += 1
+        if is_prime(i):
+            prime_count += 1
+        
+    return i
+
 def binom(n, r):
     """Returns 'n choose r'. """
     return (factorial(n) / ((factorial(r) * factorial(n - r))))
@@ -47,3 +57,27 @@ def word_score(name):
     for c in name:
         score += c_scores[c]
     return score
+
+def is_pent(n):
+    # n = (3 * x**2 - x)/2
+    # for x to be int, (1 + sqrt(24n + 1)/2 
+    # must be int
+    pen_test = (1 + sqrt(24*n + 1))/6
+    if pen_test == int(pen_test):
+        return True
+    return False
+
+def is_tri(n):
+    # Same reasoning as is_pent
+    # means (-1 + sqrt(1 + 8n))/2 must be int
+    tri_test = (-1 + sqrt(1 + 8*n))/2
+    if tri_test == int(tri_test):
+        return True
+    return False
+
+def is_hex(n):
+    # See tri and pent explanation
+    hex_test = (1 + sqrt(1 + 8*n))/4
+    if hex_test == int(hex_test):
+        return True
+    return False
