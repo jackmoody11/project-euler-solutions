20a8712ea4d1d54432bc999c5de55da4203dcaff
jackmoody11 <jackmoody@unc.edu>
2019-03-29 11:34:18 +0100

    Minor spacing change

--- python/p043.py
+++ python/p043.py
@@ -3,7 +3,6 @@ from itertools import permutations
 
 PRIMES = primes_list(7)
 
-
 def follows_pattern(n):
     for i in range(1, 8):
         if int(str(n)[i:i+3]) % PRIMES[i-1] != 0:
@@ -11,7 +10,6 @@ def follows_pattern(n):
     return True
 
 def compute():
-    
     pattern_sum = 0
     nine_pandigitals = [''.join(p) for p in permutations('0123456789') if p[0] != '0']
     for pandigital in nine_pandigitals:
