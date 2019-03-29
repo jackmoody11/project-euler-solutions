f3d35e6e4e342fae861ee2fc5491e6aa6212777e
jackmoody11 <jackmoody@unc.edu>
2019-03-29 22:52:09 +0100

    Solve #47 (needs speed improvements before adding to answers.txt for testing)

--- python/p047.py
+++ python/p047.py
@@ -1,14 +1,26 @@
-from utils import factors
+from utils import primes
 
-def unique_prime_factors(n):
-    return [len(set(factors(i))) for i in range(1, n)]
+CACHE = primes(500_000)
+
+def unique_prime_factors_count(n, x):
+    """ Return set of prime factors for integer n. """
+    prime_factors = set()
+    i = 0
+    while n != 1:
+        while n % CACHE[i] == 0:
+            n /= CACHE[i]
+            prime_factors.add(CACHE[i])
+        i += 1
+        if len(prime_factors) > x:
+            return False
+    return len(prime_factors) == x
 
 
 def compute():
-    num_prime_factors = unique_prime_factors(1_000_000)
-    for idx, val in enumerate(num_prime_factors):
-        if all(num_prime_factors[idx + j] == 4 for j in range(4)):
-            return idx + 1
+    v = 210
+    while not all(unique_prime_factors_count(v + i, 4) for i in range(4)):
+        v += 1
+    return v
 
 if __name__ == "__main__":
     print(compute())
