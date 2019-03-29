8b8fb06a2e77f26946e75ec53920344aa36dfe9e
jackmoody11 <jackmoody@unc.edu>
2019-03-29 11:30:49 +0100

    Add explanation

--- python/p046.py
+++ python/p046.py
@@ -3,6 +3,8 @@ from math import sqrt
 
 def compute():
     # We are given that this pattern holds at least until 33
+    # We will cheat a bit here and assume that this pattern will not hold until 10,000
+    # This can be found with some experimentation
     prime_list_full = primes(10000)
     num = 35
     while True:
