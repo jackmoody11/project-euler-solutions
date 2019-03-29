ce6289d38984169e5cf5363733f0d36184bf6eaf
jackmoody11 <jackmoody@unc.edu>
2019-03-29 11:28:38 +0100

    Add function docstring

--- python/p012.py
+++ python/p012.py
@@ -4,18 +4,17 @@ from math import sqrt
 
 
 def num_divisors(n):
-	end = int(sqrt(n))
-    # ex: 10 has divisors 1 2 5 10
-    # 1 2 | 5 10 -> 2 captured * 2 = 4 divisors
-    # ex: 16 has divisor 1 2 | 4 8 16 -> 3 captured * 2 = 6,
-    # but 4 also be counted once
-
-	result = sum(2
-		for i in range(1, end)
-		if n % i == 0)
-	if end**2 == n:
-		result += 1
-	return result
+    """
+    ex: 10 has divisors 1 2 5 10
+    1 2 | 5 10 -> 2 captured * 2 = 4 divisors
+    ex: 16 has divisor 1 2 | 4 8 16 -> 3 captured * 2 = 6,
+    but 4 also be counted once
+    """
+    end = int(sqrt(n))
+    result = sum(2 for i in range(1, end) if n % i == 0)
+    if end**2 == n:
+        result += 1
+    return result
 
 
 def compute():
