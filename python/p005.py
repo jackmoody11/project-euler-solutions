59075cf5549b02520238e3cca856e88c07eab527
jackmoody11 <jackmoody@unc.edu>
2019-03-29 13:58:09 +0100

    Use LCM function for #5

--- python/p005.py
+++ python/p005.py
@@ -1,11 +1,13 @@
 # Find the smallest number that is divisible by the numbers 1, 2, ..., 20
-from utils import prod
+from utils import lcm
+
 
 def compute():
     # Find the greatest number of prime factors
-    # for each prime and multiply them
+    # for each prime and multiply them (find the least common multiple).
     # By hand, this gives prod([2**4, 3**2, 5, 7, 11, 13, 17, 19])
-    return prod([2**4, 3**2, 5, 7, 11, 13, 17, 19])
+    # We will use the lcm function from utils here for clarity
+    return lcm(*range(2, 20))
 
 
 if __name__ == '__main__':
