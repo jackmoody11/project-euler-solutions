7939b5aab9ff1a42a278346d1a7d73e55e9da74a
jackmoody11 <jackmoody@unc.edu>
2019-03-29 11:14:08 +0100

    Add testing

---
+++ python/tests.py
@@ -0,0 +1,25 @@
+import importlib, time
+
+
+filename = "../answers.txt"
+
+with open(filename, 'r') as f:
+    lines = [line.strip() for line in f.readlines()]
+    ANSWERS = {int(line.split(". ")[0]): int(line.split(". ")[1]) for line in lines}
+
+def main():
+	total_time = 0.0  # In seconds
+	for (prob, expected) in sorted(ANSWERS.items()):
+		module = importlib.import_module("p{:03d}".format(prob))
+		starttime = time.time()
+		actual = int(module.compute())  # Must return an int
+		elapsedtime = time.time() - starttime
+		total_time += elapsedtime
+		print("Problem {:03d}: {:7d} ms{}".format(
+			prob, int(round(elapsedtime * 1000)),
+			"" if actual == expected else "    *** FAIL ***"))
+	print("Total computation time: {} ms".format(int(round(total_time * 1000))))
+
+
+if __name__ == "__main__":
+    main()
