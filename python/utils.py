f3443cb28473c26dd6b88de8b628a9ec24e95e51
jackmoody11 <jackmoody@unc.edu>
2019-03-31 01:10:24 +0100

    Update docstrings

--- python/utils.py
+++ python/utils.py
@@ -14,6 +14,10 @@ def prod(l):
     """
     Takes an iterable and returns product of numbers in iterable.
     Will return 1 if iterable is empty.
+    >>> prod(range(1,5))
+    24
+    >>> prod([2,3,5])
+    30
     """
     p = 1
     for arg in l:
@@ -21,8 +25,12 @@ def prod(l):
     return p
 
 def binom(n, r):
-    """Returns 'n choose r'. """
-    return (factorial(n) / ((factorial(r) * factorial(n - r))))
+    """
+    Returns 'n choose r'.
+    >>> binom(5, 2)
+    10.0
+    """
+    return factorial(n) // ((factorial(r) * factorial(n - r)))
 
 def lcm(*nums):
     """
@@ -47,7 +55,13 @@ def lcm(*nums):
 ########################################
 
 def is_prime(n):
-    """ Test if integer n is prime. """
+    """
+    Test if integer n is prime.
+    >>> is_prime(25)
+    False
+    >>> is_prime(11)
+    True
+    """
     # Negative numbers don't count as primes
     if n < 2:
         return False
@@ -64,7 +78,9 @@ def is_prime(n):
 def primes_list(n):
     """
     Return first n primes. 
-    Different from primes function which returns primes less than n.
+    Different from primes function.
+    >>> primes_list(5)
+    [2, 3, 5, 7, 11]
     """
     if n == 1:
         return [2]
@@ -81,7 +97,11 @@ def primes_list(n):
     return p_list
 
 def primes(n):
-    """ Returns  a list of primes < n """
+    """
+    Returns  a list of primes < n
+    >>> primes(10)
+    [2, 3, 5, 7]
+    """
     sieve = [True] * n
     for i in range(3,int(n**0.5)+1,2):
         if sieve[i]:
@@ -89,7 +109,11 @@ def primes(n):
     return [2] + [i for i in range(3,n,2) if sieve[i]]
 
 def prime_cache(n):
-    """ True/False list of primes from 0 to n-1 """
+    """
+    True/False list of primes from 0 to n-1
+    >>> prime_cache(10)
+    [False, False, True, True, False, True, False, True, False, False]
+    """
     sieve = [False, False] + [True] * (n-2)
     sieve[4::2] = [False] * ((n-3)//2)
     for i in range(3,int(n**0.5)+1,2):
@@ -115,7 +139,15 @@ def word_score(name):
 ########################################
 
 def is_palindrome(s):
-    """ Test whether or not string or int is palindrome """
+    """
+    Test whether or not string or int is palindrome
+    >>> is_palindrome(121)
+    True
+    >>> is_palindrome(125)
+    False
+    >>> is_palindrome('121')
+    True
+    """
     if isinstance(s, str):
         if s == s[::-1]:
             return True
@@ -133,7 +165,13 @@ def digit_permutations(n):
     return set([int(''.join(p)) for p in permutations(str(n)) if p[0] != '0'])
 
 def count_digits(n):
-    """ Count digits in positive integer n. """
+    """
+    Count digits in positive integer n.
+    >>> count_digits(125)
+    3
+    >>> count_digits(2)
+    1
+    """
     count = 0
     while n >= 1:
         count += 1
@@ -150,13 +188,30 @@ def is_pandigital(n):
             return False
     return True
 
+def digital_sum(n):
+    """
+    Returns sum of digits of integer
+    >>> digital_sum(123)
+    6
+    """
+    total = 0
+    while n != 0:
+        total += n % 10
+        n //= 10
+    return total
 
 ########################################
 ############### Factors ################
 ########################################
 
 def factors(n):
-    """ Return list of prime factors for integer n. """
+    """
+    Return list of prime factors for integer n.
+    >>> factors(35)
+    [5, 7]
+    >>> factors(24)
+    [2, 2, 2, 3]
+    """
     potential_factors = primes(n + 1)
     prime_factors = []
     i = 0
@@ -173,24 +228,43 @@ def factors(n):
 ########################################
 
 def is_pent(n):
-    # n = (3 * x**2 - x)/2
-    # for x to be int, (1 + sqrt(24n + 1)/2 
-    # must be int
+    """
+    n = (3 * x**2 - x)/2
+    for x to be int, (1 + sqrt(24n + 1)/2 
+    must be int
+    >>> is_pent(5)
+    True
+    >>> is_pent(6)
+    False
+    """
     pen_test = (1 + sqrt(24*n + 1))/6
     if pen_test == int(pen_test):
         return True
     return False
 
 def is_tri(n):
-    # Same reasoning as is_pent
-    # means (-1 + sqrt(1 + 8n))/2 must be int
+    """
+    Same reasoning as is_pent
+    means (-1 + sqrt(1 + 8n))/2 must be int
+    >>> is_tri(3)
+    True
+    >>> is_tri(5)
+    False
+    """
     tri_test = (-1 + sqrt(1 + 8*n))/2
     if tri_test == int(tri_test):
         return True
     return False
 
 def is_hex(n):
-    # See tri and pent explanation
+    """
+    Test if number is hexagonal number.
+    That is, of the form 2n * (2n-1)/2.
+    >>> is_hex(6)
+    True
+    >>> is_hex(11)
+    False
+    """
     hex_test = (1 + sqrt(1 + 8*n))/4
     if hex_test == int(hex_test):
         return True
