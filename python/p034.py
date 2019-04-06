df75d14c29f601ecb8cb89d979305ae3f4411818
jackmoody11 <jackmoody@unc.edu>
2019-04-06 23:35:15 +0200

    Load dictionary of factorial values to save computation time

--- python/p034.py
+++ python/p034.py
@@ -1,18 +1,20 @@
 # Once we get to a decimal place where adding 9! = 362,880
 # can't keep up, we will not be able to get curious numbers
 # that are any larger
-# Since 7 * 9! < 9,999,999, we can limit our search to be 
+# Since 7 * 9! < 9,999,999, we can limit our search to be
 # below 7 * 9! = 2,540,160
-from math import factorial as f
+FACTORIALS = {0: 1, 1: 1, 2: 2, 3: 6, 4: 24,
+              5: 120, 6: 720, 7: 5_040, 8: 40_320, 9: 362_880}
 
 
 def compute():
     total = 0
     # Do not include 1! or 2! since they are not sums
     for i in range(3, 2_540_160):
-        if i == sum([f(int(c)) for c in str(i)]):
+        if i == sum([FACTORIALS[int(c)] for c in str(i)]):
             total += i
     return total
+
 
 if __name__ == '__main__':
     print(compute())
