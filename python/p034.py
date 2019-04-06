c268124977491c9e86453b55015769579ae3d243
jackmoody11 <jackmoody@unc.edu>
2019-04-07 00:09:45 +0200

    Further minimize upper bound with some manual checking

--- python/p034.py
+++ python/p034.py
@@ -5,19 +5,20 @@
 # below 7 * 9! = 2,540,160
 # Since 2! + 6 * 9! = 2,177,282 < 2,999,999, we know that no number
 # will exceed this either
+# Further, since 2! + 0! + 5*9! < 2,000,000 and 1! + 6*9! is the
+# first number which at least reaches its own value (1,999,999),
+# we can set 1,999,999 as our upper bound
 FACTORIALS = {0: 1, 1: 1, 2: 2, 3: 6, 4: 24,
               5: 120, 6: 720, 7: 5_040, 8: 40_320, 9: 362_880}
 
 
 def compute():
     total = 0
-    nums = set()
     # Do not include 1! or 2! since they are not sums
-    for i in range(3, 2_177_283):
+    for i in range(3, 2_000_000):
         if i == sum([FACTORIALS[int(c)] for c in str(i)]):
             total += i
-            nums.add(i)
-    return total, max(nums)
+    return total
 
 
 if __name__ == '__main__':
