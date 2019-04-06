cf6a1463e29db750ab553c4532cbaf7e1774a47b
jackmoody11 <jackmoody@unc.edu>
2019-04-07 00:57:27 +0200

    Rewrite unique prime factor function for massive performance improvements

--- python/p047.py
+++ python/p047.py
@@ -1,27 +1,36 @@
-# Needs to be optimized further
-from utils import primes
-
-CACHE = primes(500_000)
-
-def unique_prime_factors_count(n, x):
+def unique_prime_factors(n):
     """ Return set of prime factors for integer n. """
     prime_factors = set()
-    i = 0
-    while n != 1:
-        while n % CACHE[i] == 0:
-            n /= CACHE[i]
-            prime_factors.add(CACHE[i])
-        i += 1
-        if len(prime_factors) > x:
-            return False
-    return len(prime_factors) == x
+    i = 2
+    # Get prime factors using prime factorization from bottom up (2, 3, 5, ...)
+    while i < n**0.5:
+        if n % i == 0:
+            n /= i
+            prime_factors.add(i)
+        else:
+            i += 1
+    return len(prime_factors) + 1
 
 
 def compute():
-    v = 210
-    while not all(unique_prime_factors_count(v + i, 4) for i in range(4)):
-        v += 1
-    return v
+    """
+    Minimum for j is 2 * 3 * 5 * 7 = 210
+    """
+    j = 210
+    while True:
+        
+        count = 0
+        while unique_prime_factors(j) == 4 and count != 4:
+            count += 1
+            j += 1
+        if count == 4:
+            # Added one extra j in loop at the end
+            j -= 1
+            break
+        else:
+            j += 1
+    return j - 3
+
 
 if __name__ == "__main__":
     print(compute())
