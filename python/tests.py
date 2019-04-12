eb2876404a97e5ad86748ac575bd0427d7ff7c48
jackmoody11 <jackmoody@unc.edu>
2019-04-12 23:05:23 +0200

    Allow tests to take arguments from command line for partial testing.

--- python/tests.py
+++ python/tests.py
@@ -1,5 +1,6 @@
 import importlib, time
 import doctest
+import sys
 
 filename = "../answers.txt"
 
@@ -7,19 +8,34 @@ with open(filename, 'r') as f:
     lines = [line.strip() for line in f.readlines()]
     ANSWERS = {int(line.split(". ")[0]): int(line.split(". ")[1]) for line in lines}
 
-def main(doctests=False):
-	total_time = 0.0  # In seconds
-	for (prob, expected) in sorted(ANSWERS.items()):
+def test(prob, expected):
+	try:
 		module = importlib.import_module("p{:03d}".format(prob))
-		starttime = time.time()
-		actual = int(module.compute())  # Must return an int
-		elapsedtime = time.time() - starttime
-		total_time += elapsedtime
-		print("Problem {:03d}: {:7d} ms{}".format(
-			prob, int(round(elapsedtime * 1000)),
-			"" if actual == expected else "    *** FAIL ***"))
+	except ModuleNotFoundError:
+		raise ModuleNotFoundError("It looks like you haven't solved #{prob} yet".format(prob=prob))
+	start = time.time()
+	actual = int(module.compute())  # Must return an int
+	elapsed = time.time() - start
+	print("Problem {:03d}: {:7d} ms{}".format(
+		prob, int(round(elapsed * 1000)),
+		"" if actual == expected else "    *** FAIL ***"))
+
+def main(doctests=False):
+	start = time.time()  # In seconds
+	if len(sys.argv) > 1:
+		for prob in list(map(int, sys.argv[1:])):
+			try:
+				test(prob, ANSWERS[prob])
+			except KeyError:
+				raise KeyError("It looks like you haven't added #{prob} to answers.txt yet".format(prob=prob))
+		num_probs = len(sys.argv[1:])
+	else:
+		for (prob, expected) in sorted(ANSWERS.items()):
+			test(prob, expected)
+		num_probs = len(ANSWERS)
+	total_time = time.time() - start
 	print("Total computation time: {} ms".format(int(round(total_time * 1000))))
-	print("{} problems solved".format(len(ANSWERS)))
+	print("{} problems solved".format(num_probs))
 
 	if doctests:
 		doctesting()
