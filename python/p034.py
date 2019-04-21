e6c6cf35d03b8ab1242c862ef771267a85feb52d
jackmoody11 <jackmoody@unc.edu>
2019-04-21 15:26:36 +0200

    Add early break if sum > n and use generator for sum

--- python/p034.py
+++ python/p034.py
@@ -11,15 +11,23 @@
 FACTORIALS = {0: 1, 1: 1, 2: 2, 3: 6, 4: 24,
               5: 120, 6: 720, 7: 5_040, 8: 40_320, 9: 362_880}
 
+def f_sum(n):
+    """
+    Calculate sum of factorials of digits.
+    Breaks early if sum > number.
+    """
+    factorial_sum = 0
+    i = n
+    while factorial_sum <= n:
+        if i == 0:
+            break
+        factorial_sum += FACTORIALS[i % 10]
+        i //= 10
+    return factorial_sum    
 
 def compute():
-    total = 0
     # Do not include 1! or 2! since they are not sums
-    for i in range(3, 2_000_000):
-        if i == sum([FACTORIALS[int(c)] for c in str(i)]):
-            total += i
-    return total
-
+    return sum((i for i in range(3, 2_000_000) if i == f_sum(i)))
 
 if __name__ == '__main__':
     print(compute())
