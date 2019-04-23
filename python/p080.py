291b66f892ed41b53c2d28fd51f8f944e099bfe6
jackmoody11 <jackmoody@unc.edu>
2019-04-23 15:36:49 +0200

    Simplify solution using inline for loop with sum

--- python/p080.py
+++ python/p080.py
@@ -2,12 +2,13 @@ from decimal import getcontext, Decimal
 
 def compute():
     getcontext().prec = 102 # Set accuracy to 102 places (extra 2 for whole number part)
-    total = 0
     LIMIT = 100
     not_square = [i for i in range(LIMIT + 1) if int(i**0.5) != i**0.5]
-    for i in not_square:
-        total += sum(Decimal(i).sqrt().as_tuple().digits[:100])
-    return total
+
+    return sum(
+        (sum(Decimal(i).sqrt().as_tuple().digits[:100]) for i in not_square)
+    )
+    
 
 
 if __name__ == "__main__":
