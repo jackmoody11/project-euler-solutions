dcdaa5086a6ffb9b042a57d82668e5a435540f66
jackmoody11 <jackmoody@unc.edu>
2020-01-09 19:07:03 -0500

    Fix #3 algorithm

--- python/p003.py
+++ python/p003.py
@@ -1,18 +1,25 @@
 # Compute the max prime factor of 600,851,475,143
-# It is easily verified that 71 is the 
+# It is easily verified that 71 is the
 # smallest prime divisor, and thus we only need to check
 # to sqrt of the number (it is not prime itself)
 from math import sqrt
-from utils import is_prime
-    
-TARGET = 600_851_475_143 
+from utils import is_prime, primes
+
 
 def compute():
+    TARGET = 600_851_475_143
+    PRIMES = primes(int(sqrt(TARGET)) + 1)
     max_div = 0
-    for i in range(2, int(sqrt(TARGET)) + 1):
-        if TARGET % i == 0 and is_prime(i):
-            max_div = i
-    return max_div
+    for p in PRIMES:
+        if TARGET % p == 0:
+            max_div = p
+        while TARGET % p == 0:
+            TARGET //= p
+    if TARGET > max_div and is_prime(TARGET):
+        return TARGET
+    else:
+        return max_div
+
 
 if __name__ == '__main__':
     print(compute())
