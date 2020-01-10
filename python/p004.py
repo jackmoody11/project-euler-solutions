e2d75739588ed5baf004d2d3681b0dad2f4d05a0
jackmoody11 <jackmoody@unc.edu>
2020-01-09 19:21:45 -0500

    Speed up algorithm by only checking if number is palindrome if greater than current max

--- python/p004.py
+++ python/p004.py
@@ -1,15 +1,17 @@
 # Find largest palindrome that is a multiple of 2 3 digit numbers
 from utils import is_palindrome
 
+
 def compute():
     max_p = 0
     for i in range(100, 1000):
         # Only go to i (inclusive) to eliminate double counting
         for j in range(100, i + 1):
             num = i * j
-            if is_palindrome(str(num)) and num > max_p:
+            if num > max_p and is_palindrome(str(num)):
                 max_p = num
     return max_p
+
 
 if __name__ == '__main__':
     print(compute())
